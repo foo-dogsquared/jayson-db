fc3aac4fcb06640cab62a84db1e0dc9ba3ad694e
Gabriel Arazas <christiangabrielarazas@gmail.com>
2019-03-15 20:40:18 +0800

    Add 'clone' and 'objLength' helper functions

--- src/helpers.js
+++ src/helpers.js
@@ -4,6 +4,20 @@ module.exports.generate = function(max = Number.MAX_SAFE_INTEGER) {
   return Math.floor(Math.random() * max);
 }
 
-module.exports.isSchema = function(schemaObject) {
-  return schemaObject && schemaObject instanceof schema.Schema;
+module.exports.objLength = function(object) {
+  let length = 0;
+  if (!object && typeof object !== 'object') return length;
+
+  for (const property in object) {
+    if (object.hasOwnProperty(property)) ++length;
+  }
+
+  return length;
+}
+
+module.exports.clone = function(data) {
+  if (data instanceof Object) {
+    return Object.assign({}, data);
+  }
+  else return data;
 }
